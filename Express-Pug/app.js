1795114d19928c1f25a3ce64e7efdeed5adeea01
Molodduwa <molodduwa.andri@iisgalvanimi.edu.it>
2024-02-20 12:43:41 +0000

    Lato Express finito

--- Express-Pug/app.js
+++ Express-Pug/app.js
@@ -1,13 +1,18 @@
 const express = require('express');
-const macchine = require('./macchine.json'); //Copia il file people.json dentro la variabile people
+const Acomp = require('./json/ACompatte.json'); //Copia il file people.json dentro la variabile people
+const ASuv = require('./json/ASuv.json');
+const ASpor= require('./json/ASportive.json');
 const app = express();
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
 app.get('/', (req, res) => {
 
-    res.render('index', {
+    res.render('Home', {
     title: 'Homepage',
-    macchine: macchine['Auto Compatte'] //Passa il vettore profiles alla pagina index.pug
+    compatte: Acomp['Auto Compatte'], //Passa il vettore 
+    suvs: ASuv['Auto SUV'],
+    sportive: ASpor['Auto Sportive']
+
     });
 });
 
